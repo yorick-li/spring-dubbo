f42b334d0e22768aa900ea515d2e245b5ba3a59a
yorick <yorick_li2017@163.com>
2021-11-04 10:50:27 +0800

    dubbo：重试机制

--- dubbo-service/src/main/java/com/demo/service/impl/UserServiceImpl.java
+++ dubbo-service/src/main/java/com/demo/service/impl/UserServiceImpl.java
@@ -6,7 +6,8 @@ import org.apache.dubbo.config.annotation.Service;
 
 import java.util.concurrent.TimeUnit;
 
-@Service(timeout=3000, retries=0)
+// 重试次数为：n，则会进入服务的方法：n+1 次
+@Service(timeout=3000, retries=3)
 public class UserServiceImpl implements IUserService {
 
     @Override
@@ -15,8 +16,19 @@ public class UserServiceImpl implements IUserService {
     }
 
 
+    int i = 1;
     @Override
     public User findById(int id) {
+        // 启动一个线程计数：重试次数
+        new Thread(
+            new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("第" + i++ + "次重试");
+                }
+            }
+        ).start();
+
         User user = new User(1, "zhangsan", 26);
         // 利用线程模拟超时
         try {
--- dubbo-web/src/main/java/com/demo/controller/UserController.java
+++ dubbo-web/src/main/java/com/demo/controller/UserController.java
@@ -31,20 +31,20 @@ public class UserController {
     int i = 1;
     @RequestMapping("/findUser")
     public User findById(int id) {
-        // 启动一个线程计时
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    while (i<10) {
-                        TimeUnit.SECONDS.sleep(1);
-                        System.out.println(i++);
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
+        // 启动一个线程计时：超时时间
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    while (i<10) {
+//                        TimeUnit.SECONDS.sleep(1);
+//                        System.out.println(i++);
+//                    }
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }).start();
 
         return userService.findById(id);
     }
