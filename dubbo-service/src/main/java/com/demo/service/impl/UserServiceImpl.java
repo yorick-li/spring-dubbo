1efddd0a14574ff3883061fd5c2601bc355541ed
yorick <yorick_li2017@163.com>
2021-11-04 10:41:38 +0800

    dubbo：超时机制

--- dubbo-service/src/main/java/com/demo/service/impl/UserServiceImpl.java
+++ dubbo-service/src/main/java/com/demo/service/impl/UserServiceImpl.java
@@ -4,8 +4,9 @@ import com.demo.pojo.User;
 import com.demo.service.IUserService;
 import org.apache.dubbo.config.annotation.Service;
 
-//@Service
-@Service
+import java.util.concurrent.TimeUnit;
+
+@Service(timeout=3000, retries=0)
 public class UserServiceImpl implements IUserService {
 
     @Override
@@ -13,9 +14,16 @@ public class UserServiceImpl implements IUserService {
         return "hello dubbo! I love you~";
     }
 
+
     @Override
     public User findById(int id) {
         User user = new User(1, "zhangsan", 26);
+        // 利用线程模拟超时
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return user;
     }
 
--- dubbo-web/src/main/java/com/demo/controller/UserController.java
+++ dubbo-web/src/main/java/com/demo/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -26,8 +28,24 @@ public class UserController {
      * @param id
      * @return
      */
+    int i = 1;
     @RequestMapping("/findUser")
     public User findById(int id) {
+        // 启动一个线程计时
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (i<10) {
+                        TimeUnit.SECONDS.sleep(1);
+                        System.out.println(i++);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
         return userService.findById(id);
     }
 
